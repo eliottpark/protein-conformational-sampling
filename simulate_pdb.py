2a230419e09fc4d600727dd993de09a3e57de023
Eliott Park <eliottpark@me.com>
2025-05-05 17:20:53 -0700

    base changes to simulate py

--- simulate_pdb.py
+++ simulate_pdb.py
@@ -1,28 +1,31 @@
-from simtk.openmm.app import *
-from simtk.openmm import *
+from openmm.app import *
+from openmm import *
 from simtk.unit import *
 import sys
 
 
 def main(input_file, time_length=20, temperature=300, step_size=0.002, output_file=None):
+    log = open('state.log', 'w')
     num_steps = float(time_length) * 1000 / step_size # convert ns to ps
     
     if output_file is None:
         output_file = input_file.replace('.pdb', '_output.pdb')
     pdb = PDBFile(input_file)
-    forcefield = ForceField("amber/protein.ff14SB.xml", 'amber/tip3p.xml')
+    forcefield = ForceField("amber/protein.ff14SB.xml", "tip3p.xml") 
     # forcefield = ForceField('amber99sb.xml', 'tip3p.xml')
-    system = forcefield.createSystem(pdb.topology, nonbondedMethod=PME,
-            nonbondedCutoff=10*angstrom, constraints=HBonds)
-    integrator = LangevinIntegrator(temperature*kelvin, 1/picosecond, step_size*picoseconds)
+    system = forcefield.createSystem(pdb.topology, nonbondedMethod=NoCutoff, constraints=HBonds)
+            #nonbondedCutoff=10*angstrom, constraints=HBonds)
+
+    integrator = LangevinIntegrator(float(temperature)*kelvin, 1/picosecond, float(step_size)*picoseconds)
     simulation = Simulation(pdb.topology, system, integrator)
     simulation.context.setPositions(pdb.positions)
     simulation.minimizeEnergy()
     
-    simulation.reporters.append(PDBReporter('output.pdb', 1000))
-    simulation.reporters.append(StateDataReporter(sys.stdout, 1000, step=True,
+    simulation.reporters.append(PDBReporter(output_file, 1000))
+    simulation.reporters.append(StateDataReporter(log, 1000, step=True,
             potentialEnergy=True, temperature=True))
     simulation.step(num_steps)
+    log.close()
     
 if __name__ == '__main__':
     input_file = sys.argv[1]
